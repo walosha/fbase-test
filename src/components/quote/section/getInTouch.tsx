5f47f25f6dec09a15a088903893d138b00cd3932
olawale afuye <walosha@yahoo.com>
2024-02-26 00:02:56 +0100

    added get in touch

--- src/components/quote/section/getInTouch.tsx
+++ src/components/quote/section/getInTouch.tsx
@@ -1,7 +1,15 @@
 export default function GetInTouch({ setStage }: any) {
   return (
-    <div className="animate-fade-down animate-once animate-duration-1000 animate-delay-0 animate-ease-in-out">
-      <h1 onClick={() => setStage(0)}>getInTouch</h1>
+    <div
+      onClick={() => setStage(0)}
+      className="animate-fade-down animate-once animate-duration-1000 animate-delay-0 animate-ease-in-out"
+    >
+      <div className="flex cursor-pointer flex-col text-center">
+        <h2 className="text-white font-primary text-3xl">Enquiry received.</h2>
+        <p className="text-primaryLight font-primary text-4xl">
+          We wil be in touch soon !
+        </p>
+      </div>
     </div>
   )
 }
